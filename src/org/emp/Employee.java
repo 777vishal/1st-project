afc2f116f3266870b5343aeb5cad7b1846ce03ec
777vishal <vishalshanmugam777@gmail.com>
2021-10-12 10:57:38 +0530

    project

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,32 @@
+package org.emp;
+
+public class Employee {
+	private void empId() {
+		// TODO Auto-generated method stub
+System.out.println("1234");
+	}
+private void empName() {
+	// TODO Auto-generated method stub
+System.out.println("vishal");
+}
+private void empDob() {
+	// TODO Auto-generated method stub
+System.out.println("30-01-2000");
+}
+private void empEmail() {
+	// TODO Auto-generated method stub
+System.out.println("vishal@gmail");
+}
+private void empAddress() {
+	// TODO Auto-generated method stub
+System.out.println("cuddalore");
+}
+public static void main(String[] args) {
+	Employee ep=new Employee();
+	ep.empId();
+	ep.empName();
+	ep.empDob();
+	ep.empEmail();
+	ep.empAddress();
+}
+}
